d360a4ed018460db719bdde5c93f4451e76c5325
max <max>
2024-12-12 12:19:29 +0100

    Format code and add new dependencies to pom.xml
    Refactored the main class to fix indentation issues for better readability. Added dependencies for Spring Boot web, JPA, and MySQL to support web and database functionalities in the project.

--- src/main/java/com/travelport/projecttwo/ProjectTwoApplication.java
+++ src/main/java/com/travelport/projecttwo/ProjectTwoApplication.java
@@ -6,8 +6,8 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class ProjectTwoApplication {
 
-  public static void main(String[] args) {
-    SpringApplication.run(ProjectTwoApplication.class, args);
-  }
+    public static void main(String[] args) {
+        SpringApplication.run(ProjectTwoApplication.class, args);
+    }
 
 }
