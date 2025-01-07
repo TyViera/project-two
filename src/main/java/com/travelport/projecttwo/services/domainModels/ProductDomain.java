b42ecd6eea313a4579df4d5b135f131a15679749
max <max>
2025-01-07 11:39:45 +0100

    Create product domain class

---
+++ src/main/java/com/travelport/projecttwo/services/domainModels/ProductDomain.java
@@ -0,0 +1,41 @@
+package com.travelport.projecttwo.services.domainModels;
+
+public class ProductDomain {
+
+    private String id;
+    private String code;
+    private String name;
+    private String stock;
+
+    public String getId() {
+        return id;
+    }
+
+    public void setId(String id) {
+        this.id = id;
+    }
+
+    public String getCode() {
+        return code;
+    }
+
+    public void setCode(String code) {
+        this.code = code;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getStock() {
+        return stock;
+    }
+
+    public void setStock(String stock) {
+        this.stock = stock;
+    }
+}
