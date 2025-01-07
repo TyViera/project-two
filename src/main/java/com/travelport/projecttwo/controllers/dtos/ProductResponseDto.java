bb4453a16404bed1bb2a936c037924c532acec02
max <max>
2025-01-07 11:39:17 +0100

    Create product DTOs

---
+++ src/main/java/com/travelport/projecttwo/controllers/dtos/ProductResponseDto.java
@@ -0,0 +1,32 @@
+package com.travelport.projecttwo.controllers.dtos;
+
+public class ProductResponseDto {
+
+    private String id;
+    private String code;
+    private String name;
+
+    public String getId() {
+        return id;
+    }
+
+    public void setId(String id) {
+        this.id = id;
+    }
+
+    public String getCode() {
+        return code;
+    }
+
+    public void setCode(String code) {
+        this.code = code;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+}
