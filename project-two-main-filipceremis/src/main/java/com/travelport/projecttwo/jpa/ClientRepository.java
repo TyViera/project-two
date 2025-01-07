5d412c28074660ad881d75d15e72e7a9fd72cd90
Filip Ceremis Homitchi <filip.ceremis@gmail.com>
2025-01-07 11:40:03 +0100

    Delete project-two-main-filipceremis directory

--- project-two-main-filipceremis/src/main/java/com/travelport/projecttwo/jpa/ClientRepository.java
+++
@@ -1,8 +0,0 @@
-package com.travelport.projecttwo.jpa;
-
-import com.travelport.projecttwo.entities.Client;
-import org.springframework.data.jpa.repository.JpaRepository;
-import java.util.UUID;
-
-public interface ClientRepository extends JpaRepository<Client, UUID> {
-}
