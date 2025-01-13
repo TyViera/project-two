a888808224d9f143a95685a56bd8018608d36b48
Edu Muñoz <edumunoz2002@gmail.com>
2025-01-13 12:07:44 +0100

    removed unused imports in application

--- src/main/java/com/travelport/projecttwo/ProjectTwoApplication.java
+++ src/main/java/com/travelport/projecttwo/ProjectTwoApplication.java
@@ -3,9 +3,6 @@ package com.travelport.projecttwo;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
-import io.swagger.v3.oas.annotations.OpenAPIDefinition;
-import io.swagger.v3.oas.annotations.info.Info;
-
 @SpringBootApplication
 public class ProjectTwoApplication {
 
