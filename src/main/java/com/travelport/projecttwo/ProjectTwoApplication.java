2cae89e94414a1f0558ca8deb6d5835f2f742fdd
Edu Muñoz <edumunoz2002@gmail.com>
2025-01-13 12:06:42 +0100

    removed unused @ in application

--- src/main/java/com/travelport/projecttwo/ProjectTwoApplication.java
+++ src/main/java/com/travelport/projecttwo/ProjectTwoApplication.java
@@ -6,7 +6,6 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 import io.swagger.v3.oas.annotations.OpenAPIDefinition;
 import io.swagger.v3.oas.annotations.info.Info;
 
-@OpenAPIDefinition(info = @Info(title = "Project Two", version = "v1"))
 @SpringBootApplication
 public class ProjectTwoApplication {
 
